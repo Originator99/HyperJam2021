5bdc906aca7aed71a34329c108670ad25cdcfa20
Originator99 <arsalaan.md0077@gmail.com>
2021-06-19 18:50:51 +0530

    Build settings and level editor switch

---
+++ Assets/Scripts/UI/Tutorial/ChapterWiseTutorials/TutorialSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(menuName = "Roulette/Tutorial Settings")]
+public class TutorialSettings :ScriptableObjectInstaller<TutorialSettings> {
+    public override void InstallBindings() {
+        Container.BindInstance(tutorialManagerSettings);
+        Container.BindInstance(dashboardTutorialSettings);
+        Container.BindInstance(chapter1TutorialSettings);
+    }
+
+    public TutorialManager.Settings tutorialManagerSettings;
+    public DashboardTutorials.Settings dashboardTutorialSettings;
+    public Chapter1Tutorials.Settings chapter1TutorialSettings;
+
+}
